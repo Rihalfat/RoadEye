42770468aee5d73d9666a04b464320e83792b366
Mohammed Rihal <72206851+Rihalfat@users.noreply.github.com>
2023-12-25 12:36:55 +0300

    final final with cloud

--- ROADEYE/live.py
+++ ROADEYE/live.py
@@ -1,13 +1,11 @@
 from ultralytics import YOLO
 import cv2
-# from send_email import send_email_w_attachment
 
 def predict_accident(filename):
     # Assuming YOLO is initialized inside the function
     model = YOLO("D:\RoadEye\ROADEYE\yolov8n.pt")
 
     # Start capturing video
-    # Change the source if using a different camera or video file
     cap = cv2.VideoCapture(0)
 
     # Video resolution and frame rate
@@ -16,7 +14,7 @@ def predict_accident(filename):
     fps = 8
 
     # Define the codec and create a VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Codec (change as needed)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(filename, fourcc, fps, (width, height))
 
     while True:
@@ -35,19 +33,3 @@ def predict_accident(filename):
 
     cap.release()
     cv2.destroyAllWindows()
-
-# predict_accident('1.avi')
-
-# only if accident happens
-# t = time.localtime()
-# ct = time.strftime("%H:%M", t)
-# today = date.today()
-# d = today.strftime("%B %d, %Y")
-
-# to = "mohdrihal.mr@gmail.com"
-# subject = "Accident Notification"
-# body = f"This email was sent by RoadEye at {ct} on {d}. \n An accident has been reported in the parking lot. \n Please take immediate action to address the situation.
-# Please check details attached: \n"
-# filename = "1.avi"
-
-# send_email_w_attachment(to, subject, body, filename)
--- ROADEYE/send_email.py
+++ ROADEYE/send_email.py
@@ -30,15 +30,3 @@ def send_email_w_attachment(to, subject, body, filename):
 
     server.sendmail(ec.user, to, message.as_string())
     server.quit()
-
-# t = time.localtime()
-# ct = time.strftime("%H:%M", t)
-# today = date.today()
-# d = today.strftime("%B %d, %Y")
-
-# to = "mohdrihal.mr@gmail.com"
-# subject = "Accident Notification"
-# body = f"This email was sent by RoadEye at {ct} on {d}. \n An accident has been reported in the parking lot. \n Please take immediate action to address the situation. Please check details attached: \n"
-# filename = "1.avi"
-
-# send_email_w_attachment(to, subject, body, filename)
